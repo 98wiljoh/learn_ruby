473dbe632ed50c4896574f9b54705a13cd036858
William Johansson <william.johansson98@gmail.com>
2019-01-19 22:35:56 +0100

    finished 02

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,16 @@
-#write your code here
+def add (num1, num2)
+    num1 + num2
+end
+
+def subtract (num1, num2)
+    num1 - num2
+end
+
+def sum array
+    sum = 0
+    array.each do |i|
+        sum = sum + i
+    end
+    
+    sum
+end
