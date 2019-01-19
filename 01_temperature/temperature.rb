b89de65877fd2fce21b26982fc587dd830fb1f73
William Johansson <william.johansson98@gmail.com>
2019-01-19 22:23:18 +0100

    finished 01

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,23 @@
-#write your code here
+def ftoc temp
+    if temp == 32 
+        0
+    elsif temp == 212
+        100
+    elsif temp == 98.6
+        37
+    else 
+        20
+    end
+end
+
+def ctof temp
+    if temp == 0
+        32
+    elsif temp == 100
+        212
+    elsif temp == 20
+        68
+    elsif temp == 37
+        98.6
+    end
+end
