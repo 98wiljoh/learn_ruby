da40115daf7088ca86bf35b1ba9de27cc3a900d5
William Johansson <william.johansson98@gmail.com>
2019-01-19 22:56:25 +0100

    started 02

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,21 @@
-#write your code here
+def echo message
+    message
+end
+
+def shout message
+    message.upcase
+end
+
+def repeat message
+    message + ' ' + message
+end
+
+def repeat (message, n)
+    finalMessage = ''
+
+    for i in 0..(n - 1)
+        finalMessage = finalMessage + ' ' + message 
+    end
+
+    finalMessage = finalMessage + ' ' + message
+end
