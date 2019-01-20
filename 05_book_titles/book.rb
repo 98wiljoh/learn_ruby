24c99b1916a3f4b6cf4b21afd8ea4fa24f1e56f0
William Johansson <william.johansson98@gmail.com>
2019-01-20 15:13:05 +0100

    finished book

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
-# write your code here
+    def title=(newTitle)
+        title = newTitle.split(' ').map { |word|
+            if word != 'and' && word != 'in' && word != 'the' && word != 'of' && word != 'a' && word != 'an'
+                word.capitalize
+            else
+                word
+            end
+        }
+        
+        title[0] = title[0].capitalize
+
+        @title = title.join(' ')
+    end
+
+    def title 
+        @title
+    end
 end
